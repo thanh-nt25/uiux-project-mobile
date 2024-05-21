deb77b54fecb70cf666108d7789d02bbdbc8ce8f
thanhnguyenTMNp <thanhnguyenqwe65@gmail.com>
2024-05-21 18:29:32 +0700

    add drawer color + login screen

---
+++ app/(public)/login.tsx
@@ -0,0 +1,124 @@
+import { StyleSheet, Image, Platform, Pressable, Alert, TouchableOpacity } from 'react-native';
+import { useEffect } from 'react';
+import { Text, View, TextInput } from "@/components/Themed";
+
+import { Link, usePathname } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { useAuth } from '@/context/AuthProvider';
+
+export default function login() {
+  const [username, setUsername] = useState('');``
+  const [password, setPassword] = useState('');
+
+  const pathname = usePathname();
+
+    useEffect(() => {
+        console.log(pathname);
+    }, [pathname]);
+
+  const { login } = useAuth();
+  
+  const _login = (username: string, password: string) => {
+    if (username === "" || password === "")
+      Alert.alert("Lỗi", "Hãy nhập tên đăng nhập và mật khẩu");
+    else login(username, password);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>Log in</Text>
+        <Text style={styles.subtitle}>
+          Hãy nhập tên đăng nhập và mật Khẩu bất kỳ
+        </Text>
+        <View>
+          <Text style={styles.label}>Tên đăng nhập</Text>
+          <TextInput
+            placeholder="sample"
+            style={styles.input}
+            value={username}
+            onChangeText={setUsername}
+          />
+        </View>
+        <View>
+          <Text style={styles.label}>Mật khẩu</Text>
+          <TextInput
+            placeholder="sample"
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        <Button
+          title="Đăng nhập"
+          onPress={() => _login(username, password)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const Button = ({
+  title,
+  onPress,
+  style,
+}: {
+  title: string;
+  onPress: () => void;
+  style?: any;
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      // style={[style]}
+      style={styles.button2}
+    >
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16, // px-4 in Tailwind (4 * 4 = 16)
+  },
+  title: {
+    fontSize: 24, // text-3xl in Tailwind (3 * 8 = 24)
+    marginBottom: 8, // mb-2 in Tailwind (2 * 4 = 8)
+  },
+  subtitle: {
+    marginBottom: 8, // mb-2 in Tailwind (2 * 4 = 8)
+    opacity: 0.7, // opacity-70 in Tailwind
+  },
+  label: {
+    fontWeight: '600', // font-semibold in Tailwind
+    fontSize: 18, // text-lg in Tailwind (lg = 18)
+    marginBottom: 4, // mb-1 in Tailwind (1 * 4 = 4)
+  },
+  input: {
+    borderColor: '#6B7280', // border-gray-500 in Tailwind (gray-500 hex code)
+    borderWidth: 1,
+    borderRadius: 12, // rounded-xl in Tailwind
+    padding: 12, // p-3 in Tailwind (3 * 4 = 12)
+    marginBottom: 12, // adding some margin bottom for better spacing
+  },
+  button: {
+    marginTop: 20,
+  },
+  button2: {
+    backgroundColor: '#3B82F6', // bg-blue-500 in Tailwind
+    borderRadius: 12, // rounded-xl in Tailwind
+    paddingHorizontal: 8, // px-2 in Tailwind (2 * 4 = 8)
+    paddingVertical: 12, // py-3 in Tailwind (3 * 4 = 12)
+  },
+  buttonText: {
+    color: '#FFFFFF', // text-white in Tailwind
+    textAlign: 'center', // text-center in Tailwind
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,15 +8,27 @@ import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeConfig } from '@/core/use-theme-config';
+import AuthProvider from '@/context/AuthProvider';
 
+export {
+  ErrorBoundary
+} from 'expo-router';
+
+export const unstable_settings = {
+  initialRouteName: '/login',
+};
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
   useEffect(() => {
     if (loaded) {
@@ -24,18 +36,37 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  if (!loaded) return null;
 
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
   return (
+    <AuthProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
+          <Stack.Screen name='(public)/login' options={{ headerShown: false }}/>
+          <Stack.Screen name='(drawer)' options={{ headerShown: false }}/>
+        </Stack>
+      </ThemeProvider>
+    </AuthProvider>
+  )
+}
+  
+
+{/* <Stack.Screen name="(drawer)"  options={{ headerShown: false }}/> 
+return (
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{headerShown: false}} initialRouteName='signin'>
+          <Stack.Screen name="(public)/login" options={{ headerShown: false }} />
           <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-          <Stack.Screen name="signin"  options={{ headerShown: false }}/>
-          <Stack.Screen name="+not-found" />
         </Stack>
       </ThemeProvider>
     
   );
 }
+*/}
+
+
